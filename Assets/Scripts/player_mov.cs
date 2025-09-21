43ad92ed76cf59d161c5de355c0f2fc13c868918
Mahmut Can SARI <mahmutcansari.u@gmail.com>
2025-09-21 18:18:17 +0300

    new_something

---
+++ Assets/Scripts/player_mov.cs
@@ -0,0 +1,44 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class player_mov : MonoBehaviour
+{
+    public Rigidbody2D body;
+    private Vector2 touch_s = Vector2.zero;
+    private Vector2 touch_e = Vector2.zero;
+    private Vector2 end = Vector2.zero;
+    private float com_div = Screen.width / 20;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0) 
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                touch_s = touch.position;
+                Debug.Log(touch_s);   
+            }
+            else if (touch.phase == TouchPhase.Ended) 
+            {
+                touch_e = touch.position;
+                Debug.Log(touch_e);
+                
+                end.x = (touch_e.x - touch_s.x)/com_div;
+                end.y = (touch_e.y - touch_s.y)/com_div;
+                body.linearVelocity = end;
+
+            }
+
+
+        }
+
+
+        
+    }
+}
